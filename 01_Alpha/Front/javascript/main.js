ba331bf26cf22efb539049c717eb262408cbe04a
Sammy Faraj <sf297@njit.edu>
2019-02-07 21:43:10 -0500

    Updated

--- 01_Alpha/Front/javascript/main.js
+++ 01_Alpha/Front/javascript/main.js
@@ -4,10 +4,36 @@ setInterval(function getTime(){
 	document.getElementById('date').innerHTML = timestamp;
 },100);
 
-//This will be replaced by login handling (check database for valid credentials)
-function tempError(){
-	alert("We are temporarily not accepting any logins!")
+function login(){
+	//Setup local data for sending
+	var id = document.getElementById("username").value;
+    var psw = document.getElementById("password").value;
+	var data = {"user":id, "password":psw};
+	var fdata = JSON.stringify(data);
+	//Send the local data to php file
+	var xmlhttp = new XMLHttpRequest();	
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText
+		}
+    };
+	xmlhttp.open('POST','../middle/login.php');
+	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xmlhttp.send(fdata);
+	//Receive Response 
+	if (xmlhttp.readyState === XMLHttpRequest.Done){
+		if(xmlhttp.status === 200){
+			var response = JSON.parse(xmlhttp.responseText);
+			//Chance temp variables once return variables are explecitly defined 
+			document.getElementById("NJIT").innerHTML=response.TEMPNJIT;
+			document.getElementById("DATABASE").innerHTML=response.TEMPDATABASE;
+		} 	
+		else {
+			alert('There was a problem with the request.');
+		}		
+	}
 }
+
 
 //This will soon include the addition of valid credentials to database
 function registerForm(){
