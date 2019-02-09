f6a99fb2542bc5e8a34768bab26d7618d268250d
Braulio Tonaco <edu.tonaco@gmail.com>
2019-02-08 23:34:05 -0500

    updated w/ working prototype!

--- 01_Alpha/Back/sample.php
+++ 01_Alpha/Back/sample.php
@@ -1,48 +1,55 @@
 <?php
-  echo 'HELLOOOOOOOOOOOOOOOO';
-  header("Content-type: application/json; charset=UTF-8");
-  $str_json = file_get_contents('php://input'); 
-  $response = json_decode($str_json, true);
-  if(isset($response['name'])) $name = $response['name'];
-  if(isset($response['pass'])) $pass = $response['pass'];
+$servername = "sql.njit.edu";
+$db_username = "bt74";
+$db_password = "7CkWqwkrh";
+$database = "bt74";
 
-	
-  define('dbhost', 'sql.njit.edu');
-  define('dbuser', 'jsf25');
-  define('dbpass', '8FJBGgDP');
-  define('dbtable', 'jsf25');
-  //sample logins
-  //username || password
-  //pw65 || abc
-  //test || test
-  
-  //create connection
-  $conn = new mysqli(dbhost,dbuser,dbpass,dbtable);
-  
-  //check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  else
-  {
-	  echo 'Connection Successful';
-  }
-  
-  echo $name;
-  echo $pass;
-  
-   $sql="CREATE TABLE IF NOT EXISTS `WOOOZERS` 
+// Create connection
+$conn = new mysqli($servername, $db_username, $db_password, $database);
+
+// Check connection
+if ($conn->connect_error)
+    die("<br>Connection failed: " . $conn->connect_error);
+else
+    echo "<br>Connected successfully";
+
+// sql to create table
+$sql="CREATE TABLE IF NOT EXISTS `profile`
 	(
     `NAME` varchar(255) NOT NULL default '',
     `PASS` varchar(255) NOT NULL default '',
     PRIMARY KEY  (`NAME`)
 	) ENGINE = InnoDB;";
-  
-   $stmt = mysqli_prepare($sql);
-   
-   $stmt->bind_param("sss", $name, $pass);
-   $stmt = mysql_query($stmt, $conn);
-   $stmt->execute();
-  
- 
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "<br>Table profile created successfully";
+} else {
+    echo "<br>Error creating table: " . $conn->error;
+}
+
+$str_json = file_get_contents('php://input');
+$response = json_decode($str_json, true);
+
+if(isset($response['name'])) $username = $response['name'];
+if(isset($response['pass'])) $password = $response['pass'];
+
+$sql = "SELECT * FROM profile WHERE NAME = '$username' AND PASS = '$password'";
+
+echo "<br>".$sql;
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br>username: " . $row["NAME"]. " - password: " . $row["PASS"];
+    }
+} else {
+    echo "<br>0 results";
+}
+
+$conn->close()
+
+
 ?>
