8ada0821de523cb134d242f3cae264ae0ecc2655
edinpark01 <tonaco.braulio@gmail.com>
2019-02-08 22:02:43 -0500

    Started sttempting to send data to backend

--- 01_Alpha/Middle/index.php
+++ 01_Alpha/Middle/index.php
@@ -11,4 +11,27 @@ $response = json_decode($str_json, true);
 $username = "Username";
 $password = "Password";
 
+
+
+$r = login_project($username, $password);
+
+echo $r;
+
+function login_project($name,$pass){
+    echo "<br>Running login_project";
+
+    $data = array('name' => $name,'pass' =>$pass);
+    $url = "https://web.njit.edu/~jsf25/sample.php";
+    //$url = "http://localhost:8080/Back/sample.php";
+
+    echo "<br>Calling curl_init() to URL -> ".$url."<br>";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    $response = curl_exec($ch);
+    curl_close ($ch);
+    return $response;
+}
 ?>
