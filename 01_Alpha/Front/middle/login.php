2e069dd94cadc2c7cf791eec98a6dce46f625390
edinpark01 <tonaco.braulio@gmail.com>
2019-02-09 13:54:40 -0500

    Added back login.php

---
+++ 01_Alpha/Front/middle/login.php
@@ -0,0 +1,21 @@
+<?php
+  $username=$_POST['username'];
+  $password=$_POST['password'];
+  $data = array('username' => $username,'password' =>$password);
+  //$url = "https://web.njit.edu/~jsf25/sample.php";
+  //$url = "http://localhost:8080/Back/sample.php";
+  //$url = "https://web.njit.edu/~bt74/index.php";
+  $url = "http://localhost:8080/Middle";
+  echo "<br>Calling curl_init() to URL -> ".$url."<br>";
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  $response = curl_exec($ch);
+  curl_close ($ch);
+
+  echo $response;
+
+  return $response;
+?>
