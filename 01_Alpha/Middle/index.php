39bfc2298ecfdaae9e265a6fd443e8d1afcc8234
edinpark01 <tonaco.braulio@gmail.com>
2019-02-08 21:39:14 -0500

    Added test data

--- 01_Alpha/Middle/index.php
+++ 01_Alpha/Middle/index.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: kickass
- * Date: 2019-02-08
- * Time: 20:08
- */
+echo "Running Middle";
+
+# STEP 1: Retrieve raw HTTP request data
+$str_json = file_get_contents('php://input');
+
+# STEP 2: Decode raw HTTP data into json format
+$response = json_decode($str_json, true);
+
+# STEP 3: Testdata until connection to front-end is setup
+$username = "Username";
+$password = "Password";
+
+?>
