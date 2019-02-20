acd84680c7b1444ccab755e943288acb9d98336f
edinpark01 <tonaco.braulio@gmail.com>
2019-02-19 23:13:14 -0500

    Login + teacher's main page + debug functionality

--- 02_Betta/middle/index.php
+++ 02_Betta/middle/index.php
@@ -5,46 +5,33 @@
  *  Class    CS490
  */
 
-$json_data = json_decode(
-    file_get_contents("php://input"), true
-);
+$DEBUG = 0;
 
-switch ($json_data["request_id"]) {
+if ($DEBUG) {
+    echo("<b>############ Debugging ############</b><br>");
+    $data = array(
+        "request_id" => "LOGIN",
+        "username" => "user",
+        "password" => "pass"
+    );
+} else
+    $data = json_decode(file_get_contents("php://input"), true);
+
+
+switch ($data["request_id"]) {
     case "LOGIN":
-        // TODO: Authenticate credentials with backend and return response to front-end
-        //      INPUT:
-        //          1.  username
-        //          2.  password
-        //      OUTPUT:
-        //          1.  role
-        //              1. [ 0 ] - Invalid username OR password
-        //              2. [ 1 ] - Teacher login
-        //              3. [ 2 ] - Teacher login
-        echo("Authenticate Credentials block");
-        break;
-    case "T_MAIN":
-        // TODO: Request all available test questions from backend and return response in json format to front-end
-        //      OUTPUT:
-        //          1.  questions
-        //      OUTPUT_FORMAT Example:
-        //          {
-        //              questions = [
-        //                              {
-        //                                  "id": "QUESTION UNIQUE ID"
-        //                                  "question": "SOME QUESTION",
-        //                                  "diff": "EASY"
-        //                                  "topic: "LIST"
-        //                                  "test cases": [
-        //                                                  [(input1, input2), (output)], [(input1), (output1)]
-        //                                                ]
-        //                              }
-        //                          ]
-        //          }
-        echo("Retrieve Questions block");
+        $response = array("role" => authenticate_credentials($data));
+        if ($DEBUG)
+            debug_print($data, $response, "LOGIN");
+        else
+            echo(json_encode($response));
         break;
     case "T_CREATE":
-        // TODO: Request all available test questions from backend and return response in json format to front-end
-        echo("Retrieve Questions block");
+        $response = get_all_questions($data);
+        if ($DEBUG)
+            debug_print($data, $response, "T_CREATE");
+        else
+            echo(json_encode($response));
         break;
     case "T_ADD":
         // TODO: Send added question to backend and return updated questions list from backend
@@ -67,4 +54,53 @@ switch ($json_data["request_id"]) {
         echo("Invalid request block");
 }
 
-?>
+
+function authenticate_credentials($data)
+{
+    $url = "https://web.njit.edu/~jsf25/";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    $response = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+
+    if ($GLOBALS['DEBUG'])
+        return 1;
+    else
+        return $response['role'];
+}
+
+function get_all_questions($data)
+{
+    $url = "https://web.njit.edu/~jsf25/";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    $response = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+
+    if ($GLOBALS['DEBUG'])
+        return json_decode(file_get_contents("questions.json"), true);
+    else
+        return $response;
+}
+
+function debug_print($input, $output, $case)
+{
+    switch ($case) {
+        case "LOGIN":
+            echo("<br>Retrieve Questions block");
+            break;
+        case "T_CREATE":
+            echo("<br>Retrieve Questions block");
+            break;
+    }
+    echo("<br><br>INPUT: ");
+    var_dump($input);
+    echo("<br><br>OUTPUT: ");
+    var_dump($output);
+}
