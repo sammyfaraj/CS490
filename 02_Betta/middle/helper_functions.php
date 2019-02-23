7039715af39d7c81812f1ed4da710c0a63754e0a
edinpark01 <tonaco.braulio@gmail.com>
2019-02-23 12:58:58 -0500

    Removed get all questions function

--- 02_Betta/middle/helper_functions.php
+++ 02_Betta/middle/helper_functions.php
@@ -14,20 +14,3 @@ function send_to_backend($data)
 
     return $response;
 }
-
-function get_all_questions($data)
-{
-    $url = "https://web.njit.edu/~jsf25/";
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    $response = json_decode(curl_exec($ch), true);
-    curl_close($ch);
-
-    if ($GLOBALS['DEBUG'])
-        return json_decode(file_get_contents("questions.json"), true);
-    else
-        return $response;
-}
