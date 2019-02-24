8c3898c4bcce75ebfd8fa1bdd97260e7aeb8b4c6
Junior <juniorsj@live.com>
2019-02-24 03:59:58 -0500

    Update login.php

--- 02_Betta/back/login.php
+++ 02_Betta/back/login.php
@@ -15,29 +15,32 @@
 */
 
 /*********************** 1 **********************/
-include "db.php";
+
+require_once("db.php");
+/*
 $db = new DB();
 $conn = $db->get_connection();
 
 if ($conn->connect_error)
     die("<br>Connection failed: " . $conn->connect_error);
+*/
 
 #Retrieve raw HTTP request data
-$str_json = file_get_contents('php://input');
+//$str_json = file_get_contents('php://input');
 
 #Decode raw HTTP data into json format
-$decoded_data = json_decode($str_json, true);
+//$decoded_data = json_decode($str_json, true);
 
 #Retrieve values
-$username = $decoded_data["username"];
-$password = $decoded_data["password"];
-
+$username = $data["username"];
+$password = $data["password"];
 
 /*********************** 2 **********************/
 $sql="CREATE TABLE IF NOT EXISTS `profile`
 	(
     `username` varchar(255) NOT NULL default '',
     `password` varchar(255) NOT NULL default '',
+	`role` INT,
     PRIMARY KEY  (`username`)
 	) ENGINE = InnoDB;";
 
@@ -45,34 +48,16 @@ if ($conn->query($sql) === FALSE)
     echo "<br>Table profile not created successfully";
 
 
-
-$sql = "SELECT * FROM profile WHERE NAME = '$username'";
+//password checking
+$sql = "SELECT role FROM profiles WHERE username = '$username' AND password = '$password'";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) 
 {
-	$sql_student = "SELECT * FROM profile WHERE NAME = '$username' AND PASS = '$password' AND ROLE = '1'";
-	$result = $conn->query($sql_student);
-    
-	//Credentials successful for student;
-	if ($result->num_rows > 0) 
-	{
-		$ret_json->role = "1";
-		$json_message = json_encode($ret_json);
-		echo $json_message;
-	}
-	else 
-	{ //Credentials successful for teacher;
-		$sql_teacher = "SELECT * FROM profile WHERE NAME = '$username' AND PASS = '$password' AND ROLE = '2'";
-		$result = $conn->query($sql_teacher);
-		if ($result->num_rows > 0)
-		{
-			$ret_json->role = "2";
-			$json_message = json_encode($ret_json);
-			echo $json_message;
-		}
-	}
+	$ret_json->role = $result;
+	$json_message = json_encode($ret_json);
+	echo $json_message;
 } 
 else 
 {   //Credentials are invalid;
@@ -81,6 +66,6 @@ else
 	echo $json_message;
 }
 
-$conn->close()
+//$conn->close()
 
 ?>
