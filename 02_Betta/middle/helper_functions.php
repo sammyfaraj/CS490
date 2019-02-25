c2ebabc2354e5c470029f86ef54e8836d827d5f0
edinpark01 <tonaco.braulio@gmail.com>
2019-02-24 19:24:16 -0500

    fixed decoding bug

--- 02_Betta/middle/helper_functions.php
+++ 02_Betta/middle/helper_functions.php
@@ -1,18 +1,16 @@
 <?php
 
-function send_to_backend($data)
+function send_to_backend($input_data)
     /** Sends input data from front-end to backend **/
 {
-    $url = "https://web.njit.edu/~jsf25/";
+    $url = "https://web.njit.edu/~jsf25/index.php";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($input_data));
     $response = curl_exec($ch);
     curl_close($ch);
+    return $response;
 
-    $decoded = json_decode($response, true);
-
-    return $decoded;
 }
