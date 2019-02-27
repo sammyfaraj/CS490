c4aa4e96e11d20398dbe6b6ef7d0af9bffe2ed31
edinpark01 <tonaco.braulio@gmail.com>
2019-02-26 21:07:18 -0500

    Added more test cases

--- 02_Betta/middle/test_cases.php
+++ 02_Betta/middle/test_cases.php
@@ -6,6 +6,97 @@
  */
 
 $test_cases = array(
+    "FILTER" => array(
+        "request_id" => "FILTER",
+        "topic" => "string",
+        "diff" => "easy",
+        "key" => "vowelCount"
+    ),
+    "CREATE_EXAM" => array(
+        "request_id" => "CREATE_EXAM",
+        "description" => "Creates exam in DB and set its status to 'inactive'",
+        "questions" => array(
+            array("1", "20"),
+            array("2", "15"),
+        ),
+        "semester" => "fall",
+        "year" => "2019",
+        "class" => "CS490"
+    ),
+    "GET_ALL" => array(
+        "request_id" => "GET_ALL",
+        "description" => "This case would requests for all questions in DB",
+        "expected_output" => "All questions from DB"
+
+    ),
+    "RELEASE_EXAM" => array(
+        "request_id" => "RELEASE_EXAM",
+        "description" => "Assuming we have only one exam in DB, sets exam status to available",
+        "expected_response" => array(
+            "response" => "Exam [ id ] is now available"
+        )
+    ),
+    "END_EXAM" => array(
+        "request_id" => "END_EXAM",
+        "description" => "Ends exam session, DB will set exam status to 'inactive' - GradingTool gets activated",
+        "expected_response" => array(
+            "response" => "Exam [ id ] session has ended and grades are ready for review"
+        )
+    ),
+    "REVIEW_GRADES" => array(
+        "request_id" => "REVIEW_GRADES",
+        "description" => "Requests from database all completed exams",
+        "expected_response" => array(
+            "exams" => array(
+                "Student1_exam1", "Student2_exam1", "Student3_exam1"
+            )
+        ),
+        "student_exam_format" => array(
+            "student_id" => "student_id",
+            "exam" => "exam information you guys want to output",
+            "question_1" => array("answer", "score"),
+            "question_2" => array("answer", "score")
+        )
+    ),
+    "EXAM_COMPLETED" => array(
+        "request_id" => "EXAM_COMPLETED",
+        "description" => "this is for when a student clicks on submit test",
+        "username" => "SOME STUDENT USERNAME",
+        "exam_id" => "some exam id",
+        "exam_status" => "THIS IS FOR DATABASE ONLY, this exam is unique to student profile, so set its status to REVIEW GRADE",
+        "question_id" => "1",
+        "answer_1" => " def vowelCount(test):
+                            test.lower()
+                            counter = 0
+    
+                            for chr in test:
+                                if chr in \"AEIOUaeiou\":
+                                    counter += 1
+            
+                            return counter"
+    ),
+    "GET_PENDING_EXAMS" => array(
+        "request_id" => "GET_PENDING_EXAMS",
+        "description" => "This will request from DB all students exams that need to be graded by GradingTool",
+        "expected_response" => array(
+            "username" => "student",
+            "exam_id" => "SOME EXAM ID",
+            "question_id" => "SOME QUESTION ID",
+            "answer_id" => "SOME ANSWER WHERE ID is the QUESTION ID number"
+        )
+    ),
+    "POST_TEMP_GRADES" => array(
+        "request_id" => "POST_TEMP_GRADES",
+        "description" => "When GradingTool finishes grading exams - post result onto DB",
+        "username" => "SOME STUDENT USERNAME",
+        "exam_id" => "SOME EXAM ID",
+        "question_id" => "SOME QUESTION ID",
+        "score" => "SOME CALCULATED SCORE",
+        "response" => "Exam [ id ] session has ended and grades are ready for review"
+    )
+);
+
+$completed = array(
     "LOGIN" => array(
         array(
             "request_id" => "LOGIN",
