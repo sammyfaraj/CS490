7428dde41057208e6c43f3d830107c1e51b32107
edinpark01 <tonaco.braulio@gmail.com>
2019-03-03 13:36:52 -0500

    test cases realigned so it tests in proper flow

--- 02_Betta/middle/test_cases.php
+++ 02_Betta/middle/test_cases.php
@@ -6,126 +6,6 @@
  */
 
 $test_cases = array(
-    "CREATE_EXAM" => array(
-        "request_id" => "CREATE_EXAM",
-        "exam_name" => "Spring2019",
-        "description" => "Creates exam in DB and set its status to 'active'",
-        "questions" => array(
-            array("1", "20"),
-            array("2", "15"),
-            array("3", "10")
-        ),
-
-        "expected_response" => array("response" => "Exam [ id ] Created Successfully")
-    ),
-
-    "IS_AVAILABLE" => array(
-        "request_id" => "IS_AVAILABLE",
-        "description" => "Check if there is any 'active' exam on DB, response will be either 0 or 1",
-        "expected_response" => array(
-            "active" => "1 or 0, where 1 means active, and 0 means inactive",
-        )
-    ),
-
-    "START_EXAM" => array(
-        "request_id" => "START_EXAM",
-        "description" => "Student has an available exam and want to start taking test",
-        "expected_response" => "All questions related to assigned to specific exam"
-    ),
-
-    "SEE_SCORE" => array(
-        "request_id" => "SEE_SCORE",
-        "description" => "Student is requesting to see score for an exam taken, IF EXAM STATUS is NOT 'GRADED' return 
-                         'grade not yet available' otherwise return grades",
-        "expected_response" => array(
-            array("q1", "score"),
-            array("q2", "score"),
-            array("q3", "score")
-        ),
-    ),
-
-    "REVIEW_GRADES" => array(
-        "request_id" => "REVIEW_GRADES",
-        "description" => "Requests from database all student exams WHERE status is 'AUTO_GRADED'",
-        "expected_response" => array(
-            "Student1_exam1",
-            "Student2_exam1",
-            "Student3_exam1"
-        ),
-    ),
-
-    "END_EXAM" => array(
-        "request_id" => "END_EXAM",
-        "exam_name" => "Spring2019",
-        "description" => "Ends exam session, DB will set exam status to 'inactive' - GradingTool gets activated",
-
-        "expected_response" => array(
-            "response" => "Exam [ id ] session has ended and grades are ready for review"
-        )
-    ),
-
-    "SUBMIT_EXAM" => array(
-        "request_id" => "SUBMIT_EXAM",
-        "description" => "this is for when a student clicks on submit test",
-        "username" => "student1",
-        "exam_name" => "Spring2019",
-        "exam_status" => "THIS IS FOR DATABASE ONLY, this exam is unique to student profile, so set its status to 'PENDING'",
-        "questions" => array(
-            " def vowelCount(test):
-                        test.lower()
-                        counter = 0
-
-                        for chr in test:
-                            if chr in \"AEIOUaeiou\":
-                                counter += 1
-        
-                        return counter",
-            "some answer for question 2",
-            "some answer for question 3",
-            "some answer for question 4"
-        ),
-        "expected_response" => array(
-            "response" => "Exam [exam_name] successfully submitted"
-        )
-    ),
-    "GET_PENDING_EXAMS" => array(
-        "request_id" => "GET_PENDING_EXAMS",
-        "description" => "This will request from DB all students exams that need to be graded by GradingTool",
-        "expected_response" => array(
-            "Description" => "TO BE DECIDED BY BACKEND"
-        )
-    ),
-    "POST_TEMP_GRADES" => array(
-        "request_id" => "POST_TEMP_GRADES",
-        "description" => "When GradingTool finishes grading exams - post result onto DB
-                          SETS exam status to 'AUTO_GRADED'",
-        "username" => "student1",
-        "exam_name" => "Spring2019",
-        "score" => array(
-            array("q1", "score"),
-            array("q2", "score"),
-            array("q3", "score"),
-            array("q4", "score"),
-        ),
-        "expected_response" => "Temporary Grades Successfully added to DB"
-    ),
-    "POST_FINAL_GRADES" => array(
-        "request_id" => "POST_FINAL_GRADES",
-        "description" => "When teacher finishes grading exams - post result onto DB
-                          SETS exam status to 'GRADED'",
-        "username" => "student1",
-        "exam_name" => "Spring2019",
-        "score" => array(
-            array("q1", "score"),
-            array("q2", "score"),
-            array("q3", "score"),
-            array("q4", "score"),
-        ),
-        "expected_response" => "Exam [ id OR exam_name ] for student [ student_name OR id ] grades reviewed and posted"
-    )
-);
-
-$completed = array(
     "LOGIN" => array(
         array(
             "request_id" => "LOGIN",
@@ -249,4 +129,117 @@ $completed = array(
             ),
         ),
     ),
+    "CREATE_EXAM" => array(
+        "request_id" => "CREATE_EXAM",
+        "exam_name" => "Spring2019",
+        "description" => "Creates exam in DB and set its status to 'active'",
+        "questions" => array(
+            array("1", "20"),
+            array("2", "15"),
+            array("3", "10")
+        ),
+
+        "expected_response" => array("response" => "Exam [ exam_name ] Created Successfully")
+    ),
+    "IS_AVAILABLE" => array(
+        "request_id" => "IS_AVAILABLE",
+        "description" => "Check if there is any 'active' exam on DB, response will be either 0 or 1",
+        "expected_response" => array(
+            "active" => "1 or 0, where 1 means active, and 0 means inactive",
+        )
+    ),
+    "START_EXAM" => array(
+        "request_id" => "START_EXAM",
+        "description" => "Student has an available exam and want to start taking test",
+        "expected_response" => "All questions assigned to available"
+    ),
+    "SUBMIT_EXAM" => array(
+        "request_id" => "SUBMIT_EXAM",
+        "description" => "this is for when a student clicks on submit test",
+        "username" => "student1",
+        "exam_name" => "Spring2019",
+        "exam_status" => "THIS IS FOR DATABASE ONLY, this exam is unique to student profile, so set its status to 'PENDING'",
+        "answers" => array(
+            " def vowelCount(test):
+                        test.lower()
+                        counter = 0
+
+                        for chr in test:
+                            if chr in \"AEIOUaeiou\":
+                                counter += 1
+        
+                        return counter",
+            "some answer for question 2",
+            "some answer for question 3",
+            "some answer for question 4"
+        ),
+        "expected_response" => array(
+            "response" => "Exam [exam_name] successfully submitted"
+        )
+    ),
+    "END_EXAM" => array(
+        "request_id" => "END_EXAM",
+        "exam_name" => "Spring2019",
+        "description" => "Ends exam session, DB will set exam status to 'inactive' - GradingTool gets activated",
+        "expected_response" => array(
+            "response" => "Exam [ id ] session has ended and grades are ready for review"
+        )
+    ),
+    "GET_PENDING_EXAMS" => array(
+        "request_id" => "GET_PENDING_EXAMS",
+        "description" => "This will request from DB all students exams that need to be graded by GradingTool",
+        "expected_response" => array(
+            "Description" => "TO BE DECIDED BY BACKEND"
+        )
+    ),
+    "POST_TEMP_GRADES" => array(
+        "request_id" => "POST_TEMP_GRADES",
+        "description" => "When GradingTool finishes grading exams - post result onto DB
+                          SETS exam status to 'AUTO_GRADED'",
+        "username" => "student1",
+        "exam_name" => "Spring2019",
+        "score" => array(
+            array("q1", "score"),
+            array("q2", "score"),
+            array("q3", "score"),
+            array("q4", "score"),
+        ),
+        "expected_response" => "Temporary Grades Successfully added to DB"
+    ),
+    "REVIEW_GRADES" => array(
+        "request_id" => "REVIEW_GRADES",
+        "description" => "Requests from database all student exams WHERE status is 'AUTO_GRADED'",
+        "expected_response" => array(
+            "Student1_exam1",
+            "Student2_exam1",
+            "Student3_exam1"
+        ),
+    ),
+    "POST_FINAL_GRADES" => array(
+        "request_id" => "POST_FINAL_GRADES",
+        "description" => "When teacher finishes grading exams - post result onto DB
+                          SETS exam status to 'GRADED'",
+        "username" => "student1",
+        "exam_name" => "Spring2019",
+        "score" => array(
+            array("q1", "score"),
+            array("q2", "score"),
+            array("q3", "score"),
+            array("q4", "score"),
+        ),
+        "expected_response" => "Exam [ id OR exam_name ] for student [ student_name OR id ] grades reviewed and posted"
+    ),
+    "SEE_SCORE" => array(
+    "request_id" => "SEE_SCORE",
+    "description" => "Student is requesting to see grade for an exam taken, IF EXAM STATUS is NOT 'GRADED' return 
+                         'grade not yet available' otherwise return grades",
+    "expected_response" => array(
+        array("q1", "score"),
+        array("q2", "score"),
+        array("q3", "score")
+    ),
+)
+);
+
+$completed = array(
 );
